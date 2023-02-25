84b16f6c5f82e74d40df0b7eb6b6abb31dbd8d71
samrat <majumdersamrat26@gmail.com>
2023-02-25 11:29:18 +0530

    aster

--- app.js
+++ app.js
@@ -29,3 +29,67 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// Selecting Element
+const header = document.querySelector(".header");
+const allSelector = document.querySelectorAll(".section");
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML =
+  "We use cookied for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
+header.append(message);
+// header.prepend(message.cloneNode(true));
+
+//delete element
+
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
+/*
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// Attributes
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.src);
+*/
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+  // console.log("Current scroll (X/Y)", window.pageXOffset, pageYOffset);
+  // console.log(
+  //   "height/width viewport",
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+  section1.scrollIntoView({ behavior: "smooth" });
+});
